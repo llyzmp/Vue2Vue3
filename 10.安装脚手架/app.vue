c0972b53cb7d814d787bcdf7911747e87dbb6657
zmplly <1776321381@qq.com>
2021-05-22 22:12:54 +0800

    脚手架

---
+++ 10.安装脚手架/app.vue
@@ -0,0 +1,36 @@
+<!--  -->
+<template>
+<div class='con'>
+
+  aasdasdasdasd
+</div>
+</template>
+
+<script>
+
+export default {
+components: {},
+data() {
+return {
+
+};
+},
+computed: {},
+watch: {},
+methods: {
+
+},
+created() {
+
+},
+mounted() {
+
+},
+}
+</script>
+<style  scoped>
+
+.con {
+   background-color: red;
+}
+</style>
