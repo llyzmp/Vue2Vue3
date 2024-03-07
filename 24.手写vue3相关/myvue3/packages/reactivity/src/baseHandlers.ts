31bdcac82bce2449e840c3b9d89cddfef6527778
lly <1776321381@qq.com>
2024-03-07 23:06:45 +0800

    feat: effect

--- 24.手写vue3相关/myvue3/packages/reactivity/src/baseHandlers.ts
+++ 24.手写vue3相关/myvue3/packages/reactivity/src/baseHandlers.ts
@@ -1,5 +1,6 @@
 import { isObject } from "@vue/shared";
 import { reactive } from "./reactive";
+import { track, trigger } from "./effect";
 
 export const enum ReactiveFlags {
   IS_REACTIVE = '__v_isReactive'
@@ -12,6 +13,8 @@ export const mutableHandlers = {
     if (key === ReactiveFlags.IS_REACTIVE) {
       return true
     }
+    // 收集依赖： 记录谁使用了当前属性值
+    track(target, 'get', key) // 由哪个对象的什么属性来收集依赖，activeEffect
     // 当访问对象中存在函数的情况时，函数内访问了对象中的属性，需要这样映射处理
     let result =  Reflect.get(target, key, receiver)
     // 获取对象属性的值，如果还是对象，就进一步做响应式处理
@@ -21,6 +24,10 @@ export const mutableHandlers = {
     return result
   },
   set(target, key, newValue, receiver) {
-    return Reflect.set(target, key, newValue, receiver)
+    const oldValue = target[key]
+    const result = Reflect.set(target, key, newValue, receiver)
+    // 触发更新, 让使用当前属性的代码再重新执行一遍
+    trigger(target, 'set', key, newValue, oldValue)
+    return result
   }  
 }
--- 24.手写vue3相关/myvue3/packages/reactivity/src/effect.ts
+++ 24.手写vue3相关/myvue3/packages/reactivity/src/effect.ts
@@ -1,3 +1,107 @@
-export function effect() {
-  
+export let activeEffect = undefined
+
+class ReactiveEffect {
+  public parent = null
+  public deps = []
+  public active = true
+  constructor(public fn, public scheduler) {
+
+  }
+  run() {
+    try {
+      if(!this.active) return this.fn()
+      this.parent = activeEffect
+      activeEffect = this
+      // 收集依赖之前先把依赖关系清空
+      cleanupEffect(this)
+      return this.fn()
+    } finally {
+      activeEffect = this.parent
+      this.parent = null
+    }
+  }
+  stop() {
+    if(this.active) {
+      this.active = false
+      cleanupEffect(this)
+    }
+  }
+}
+
+
+
+export function effect(fn, options: any = {}) {
+  const _effect = new ReactiveEffect(fn, options.scheduler)
+  // // 立即调用一次
+  // _effect.run()
+  // 返回函数，自定义随时调用
+  const runner = _effect.run.bind(_effect)
+  runner.effect = _effect
+  return runner
+}
+
+/**
+ * 
+ * const depsMap = new Map() 原始对象
+ * {
+ *  属性1: new Set()
+ *  属性2: new Set()
+ *  ...
+ *  属性n: new Set()
+ * }
+ * 
+ * const targetMap = new WeakMap()
+ * {
+ *  原始对象1: new Map()
+ *  原始对象2: new Map()
+ *  ...
+ *  原始对象n: new Map()
+ * }
+ */
+
+const targetMap = new WeakMap()
+export function track(target, type, key) {
+  if (activeEffect) {
+    let depsMap = targetMap.get(target)
+    if (!depsMap) {
+      depsMap = new Map()
+      targetMap.set(target, depsMap)
+    }
+    let dep = depsMap.get(key)
+    if(!dep) {
+      dep  = new Set()
+      depsMap.set(key, dep)
+    }
+
+    const shouldTrack = !dep.has(activeEffect)
+    if (shouldTrack) {
+      dep.add(activeEffect)
+      activeEffect.deps.push(dep)
+    }
+  }
+}
+// 清除依赖绑定关系
+function cleanupEffect(effect) {
+  const { deps } = effect
+  for (let i = 0; i < deps.length; i++) {
+    deps[i].delete(effect)
+  }
+  effect.deps.length = 0
+}
+export function trigger(target, type, key, newValue, oldValue) {
+  const depsMap = targetMap.get(target)
+  if(!target) return
+  let effects = depsMap.get(key)
+  if (effects) {
+    effects = new Set(effects)
+    effects.forEach(effect => {
+      if (effect !== activeEffect) {
+        if (effect.scheduler) {
+          effect.scheduler()
+        } else {
+          effect.run()
+        }
+      }
+    });
+  }
 }
